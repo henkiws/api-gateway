0d4ec38e3794ff3c90398d0029c624e0ce6b40e1
henki wisnu subakti <henki.subakti@gmail.com>
2024-11-29 20:56:06 +0700

    api media

---
+++ routes/media.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const mediaHandler = require('./handler/media')
+
+/* GET users listing. */
+router.post('/', mediaHandler.create)
+
+module.exports = router;
